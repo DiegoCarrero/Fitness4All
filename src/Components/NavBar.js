3f4a4ffb38dd779054ed02b82e484f5817bb6b22
Diegocarrero <diegocar2413@gmail.com>
2023-05-17 18:31:31 -0400

    add links to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 
 
 export default function NavBar() {
   return (
-    <div>
-      
+    <div className='NavBar'>
+      <h1>
+        {/* <img src={logo} alt='logo'/> */}
+        <Link to='/'>Fitness4All</Link>
+      </h1>
+      <h2>
+        <Link to='/snacks'>All Exercises</Link>
+      </h2>
+      <button>
+        <Link to='/snacks/new'>Add an Exercise</Link>
+      </button>
     </div>
   )
 }
