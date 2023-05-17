05030b7f6d597d7afc9a412ee8aae4797c6ffe31
Diegocarrero <diegocar2413@gmail.com>
2023-05-17 18:26:57 -0400

    export and import components and add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import Exercises from './Components/Exercises';
+import ExerciseDetails from './Components/ExerciseDetails';
+import EditExercise from './Components/EditExercise';
+import NewExercise from './Components/NewExercise';
+import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      
+      <NavBar />
       <Routes>
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/exercises' element={<Exercises />} />
+        <Route path='/exercises/:id' element={<ExerciseDetails />} />
+        <Route path='/exercises/:id/edit' element={<EditExercise />} />
+        <Route path='/exercises/new' element={<NewExercise />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/GroupFilters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GroupFilters() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
