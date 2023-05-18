b55e1bd7bab3a43a4ee85ae514d5fa1f2939e4a2
Diegocarrero <diegocar2413@gmail.com>
2023-05-18 17:01:14 -0400

    add state, fetch and map to Exercises

--- src/Components/Exercises.js
+++ src/Components/Exercises.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Exercise from "./Exercise";
+
+const API = process.env.REACT_APP_API_URL;
 
 export default function Exercises() {
+
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API}/exercises`)
+    .then((response) => setExercises(response.data))
+    .catch((error) => console.warn(error))
+  }, [])
+
+
   return (
     <div>
-      
+      {
+        exercises.map((exercise) => {
+            return <Exercise key={exercise.id} exercise={exercise} />
+        })
+      }
     </div>
   )
 }
