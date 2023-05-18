da62cfc2fb77d8198999b98f1817bb25d644f78a
Diegocarrero <diegocar2413@gmail.com>
2023-05-18 17:10:47 -0400

    pass prop and add link and content to exercise

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
-export default function Exercise() {
+export default function Exercise({ exercise }) {
+
   return (
-    <div>
-      
-    </div>
+    <article>
+      <h4>{exercise.name}</h4>
+      <img src={exercise.image} alt={exercise.name} />
+      <Link to={`/exercises/${exercise.id}`}><button>View Details</button></Link>
+    </article>
   )
 }
