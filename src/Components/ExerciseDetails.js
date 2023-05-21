77d338ba98413f7df0197c450839135448f3f5a1
Diegocarrero <diegocar2413@gmail.com>
2023-05-21 11:58:21 -0400

    add back, edit and delete buttons to show page

--- src/Components/ExerciseDetails.js
+++ src/Components/ExerciseDetails.js
@@ -11,12 +11,22 @@ export default function ExerciseDetails() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get(`${API}/bookmark/${id}`)
+    axios.get(`${API}/exercises/${id}`)
       .then((response) => {
         console.log(response.data);
         setExercise(response.data);
       }).catch((e) => console.warn('catch', e))
   }, [id, API])
+
+  function deleteExercise() {
+    axios.delete(`${API}/exercises/${id}`)
+      .then(() => {
+        navigate('/exercises');
+      },
+      (error) => console.error(error)
+    )
+    .catch((c) => console.warn('catch', c))
+  }
 
   return (
     <div>
@@ -26,6 +36,12 @@ export default function ExerciseDetails() {
       <h3>Targeted Muscles: {exercise.targeted_muscles}</h3>
       <h3>Training Level: {exercise.difficulty_level}</h3>
       <p>{exercise.how_to}</p>
+
+      <div className="show-navigation">
+        <Link to='/exercises'><button>Back</button></Link>
+        <Link to={`/exercises/${id}/edit`}><button>Edit Exercise</button></Link>
+        <button onClick={() => deleteExercise()}>Delete Exercise</button>
+      </div>
     </div>
   )
 }
